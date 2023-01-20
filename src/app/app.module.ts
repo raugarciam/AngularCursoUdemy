3a7e731274feeed00535d96ea0e3911ca778982b
Raul Garcia Mateos <raugarciam@gmail.com>
2023-01-19 21:29:06 -0600

    Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ContadorModule } from './contador/contador.module';
+
 import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ContadorModule, HeroesModule],
+  imports: [BrowserModule, HeroesModule, ContadorModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { HeroeComponent } from './heroe/heroe.component';
 import { ListadoComponent } from './listado/listado.component';
 
 @NgModule({
-  declarations: [ListadoComponent],
-  exports: [ListadoComponent],
+  declarations: [HeroeComponent, ListadoComponent],
+  exports: [ListadoComponent, HeroeComponent],
   imports: [CommonModule],
-  providers: [],
 })
 export class HeroesModule { }
